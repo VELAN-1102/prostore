c64e52dbb8e9888daa6a3b096b0c78e4084dece8
VELAN-1102 <velanvela360@gmail.com>
2025-11-11 16:45:13 +0530

    Project Completed

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -33,13 +33,14 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
             <Link href={`/product/${product.slug}`}>
               <div className='relative mx-auto'>
                 <Image
-                  src={product.banner!}
-                  alt={product.name}
-                  height='0'
-                  width='0'
-                  sizes='100vw'
-                  className='w-full h-auto'
-                />
+  src={product.banner || '/images/fallback.png'}
+  alt={product.name}
+  width={1200}
+  height={600}
+  sizes="100vw"
+  className="w-full h-auto"
+/>
+
                 <div className='absolute inset-0 flex items-end justify-center'>
                   <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
                     {product.name}
